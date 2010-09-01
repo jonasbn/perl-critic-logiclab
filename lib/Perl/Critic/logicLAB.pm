921d567a3fb63f0d2783f9dd5f03637981f4bb39
jonasbn <jonasbn@logiclab.dk>
2010-09-01 13:15:14 +0000

    PCLL-1 #time 30m #comment Created initial skeleton, we just need some tests and some POD #resolve
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/PCLL/trunk@7152 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ lib/Perl/Critic/logicLAB.pm
@@ -0,0 +1,65 @@
+package Perl::Critic::logicLAB;
+
+# $Id$
+
+# Courtesy of Adam Kennedy
+# http://search.cpan.org/dist/Task/lib/Task.pm
+
+use strict;
+
+use vars qw{$VERSION};
+
+BEGIN {
+    $VERSION = '0.01';
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::Critic::logicLAB - a set of Perl::Critic policies implemented and used by logicLAB
+
+=head1 SYNOPSIS
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Perl::Critic>
+
+=item * L<Task>
+
+=back
+
+=head1 RESOURCES
+
+=over
+
+=item * Homepage/Wiki: L<http://logiclab.jira.com/wiki/display/PCLL/Home>
+
+=item * Bugtracker: L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-logicLAB>
+
+=item * Repository: L<http://logicLAB.jira.com/svn/PCLL>
+
+=item * Project Changelog: L<http://logiclab.jira.com/browse/PCLL#selectedTab=com.atlassian.jira.plugin.system.project%3Achangelog-panel>
+
+=item * Project Site: L<http://logicLAB.jira.com/browse/PCLL>
+
+=back
+
+=head1 AUTHOR
+
+Jonas B. Nielsen (jonasbn), C<< <jonasbn@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Jonas B. Nielsen (jonasbn), All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
