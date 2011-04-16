f783d069038cd05f81dc372add97bb14147a28d2
jonasbn <jonasbn@logiclab.dk>
2011-04-16 18:28:24 +0000

    Closing 0.04 release
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/PCLL/tags/Release-0.04@7596 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Perl/Critic/logicLAB.pm
+++ lib/Perl/Critic/logicLAB.pm
@@ -10,7 +10,7 @@ use strict;
 use vars qw{$VERSION};
 
 BEGIN {
-    $VERSION = '0.03';
+    $VERSION = '0.04';
 }
 
 1;
@@ -25,7 +25,7 @@ Perl::Critic::logicLAB - a set of Perl::Critic policies implemented and used by 
 
 =head1 VERSION
 
-This documentation describes version 0.02
+This documentation describes version 0.04
 
 =head1 SYNOPSIS
 
@@ -53,71 +53,19 @@ by policy level, using the same schema
 
 =head1 POLICIES
 
-Currently Perl::Critic::logicLAB consists of the following policies.
-
 =over
 
-=item * L<Perl::Critic::Policy::logicLAB::ProhibitUseLib>
+=item * L<Perl::Critic::Policy::logicLAB::ProhibitShellDispatch|Perl::Critic::Policy::logicLAB::ProhibitShellDispatch>
 
-=item * L<Perl::Critic::Policy::logicLAB::RequireVersionFormat>
+=item * L<Perl::Critic::Policy::logicLAB::ProhibitUseLib|Perl::Critic::Policy::logicLAB::ProhibitUseLib>
 
-=back
-
-=head1 CONFIGURATION
-
-Please see the separate polices for configuration.
-
-=head1 DEPENDENCIES AND REQUIREMENTS
-
-None currently.
-
-=head1 INCOMPATIBILITIES
-
-This distribution holds no known incompatibilities.
-
-=head1 BUGS AND LIMITATIONS
-
-The distribution has no known bugs or limitations.
-
-=head1 BUG REPORTING
-
-Please use Requets Tracker for bug reporting:
-
-    http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-logicLAB
-
-=head1 SEE ALSO
-
-=over
-
-=item * L<Perl::Critic>
-
-=item * L<Task>
+=item * L<Perl::Critic::Policy::logicLAB::RequireVersionFormat|Perl::Critic::Policy::logicLAB::RequireVersionFormat>
 
 =back
-
-=head1 RESOURCES
-
-=over
-
-=item * Homepage/Wiki: L<http://logiclab.jira.com/wiki/display/PCLL/Home>
-
-=item * Bugtracker: L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-logicLAB>
-
-=item * Repository: L<http://logicLAB.jira.com/svn/PCLL>
-
-=item * Project Changelog: L<http://logiclab.jira.com/browse/PCLL#selectedTab=com.atlassian.jira.plugin.system.project%3Achangelog-panel>
-
-=item * Project Site: L<http://logicLAB.jira.com/browse/PCLL>
-
-=back
-
-=head1 AUTHOR
-
-Jonas B. Nielsen (jonasbn), C<< <jonasbn@cpan.org> >>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2010 Jonas B. Nielsen (jonasbn), All Rights Reserved.
+Copyright 2010-2011 Jonas B. Nielsen (jonasbn), All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
